c168c007e7a97df6b876d457ed7e844140c3ce27
Nilesh Singh <nileshsomvansi@gmail.com>
2021-06-05 20:21:06 +0530

    optimised code

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -43,6 +43,23 @@
             var day = addZero(date.getDate());
             var mon = addZero(date.getMonth());
             var year = date.getFullYear();
+            var thehours = date.getHours();
+      var themessage;
+      var morning = ('Good morning');
+      var afternoon = ('Good afternoon');
+      var evening = ('Good evening');
+
+      if (thehours >= 0 && thehours < 12) {
+        themessage = morning;
+
+      } else if (thehours >= 12 && thehours < 17) {
+        themessage = afternoon;
+
+      } else if (thehours >= 17 && thehours < 24) {
+        themessage = evening;
+      }
+
+      $('.greeting').append(themessage);
 
             $('.digital-clock').text("Date " + day + "/" + mon + "/" + year + " Time : " + h + ':' + m + ':' + s)
         }
@@ -80,22 +97,6 @@
 
     </script>
     <script>
-        var thehours = new Date().getHours();
-	var themessage;
-	var morning = ('Good morning');
-	var afternoon = ('Good afternoon');
-	var evening = ('Good evening');
 
-	if (thehours >= 0 && thehours < 12) {
-		themessage = morning;
-
-	} else if (thehours >= 12 && thehours < 17) {
-		themessage = afternoon;
-
-	} else if (thehours >= 17 && thehours < 24) {
-		themessage = evening;
-	}
-
-	$('.greeting').append(themessage);
     </script>
 </x-app-layout>
